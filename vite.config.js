6d186dc51c891e4a8397bf196a6d216d9652e4c3
Kanimet <kanimet200127@gmail.com>
2023-08-04 23:47:48 +0300

    All necessary dependencies are installed (frontend)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
     plugins: [
+        svgr(),
         react(),
         laravel({
             input: [
